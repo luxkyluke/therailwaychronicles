d3cd9ef80e5456c54b475c8634995d078a4c6489
luxkyluke <rock_on77500@hotmail.fr>
2016-12-18 17:07:39 +0100

    v6.5

--- scripts/load.js
+++ scripts/load.js
@@ -154,8 +154,7 @@ function affArticle(name){
 			break;
 
 		case "sacha":
-			load_template_page("sacha", "Le Voyage de Sacha", function(){
-				
+			load_template_page("sacha", "Le Voyage de Sacha", function(){				
 				replaceSachaDots();
 				setTimeout(function(){
 				    $(".se-pre-con").removeClass("visible");
@@ -227,22 +226,22 @@ function affArticle(name){
 			break;
 
 		case "article":
-			loadArticle();
+			loadArticle("article", "Blue Train");
 			break;
 		case "El_Chepe":
-			loadArticle();
+			loadArticle("El_Chepe", "El Chepe");
 			break;
 		case "Blue_Train":
-			loadArticle();
+			loadArticle("Blue_Train", "Blue Train");
 			break;
 		case "Hiram_Bingham":
-			loadArticle();
+			loadArticle("Hiram_Bingham", "Hiram Bingham");
 			break;
 		case "Petite_Ceinture":
-			loadArticle();
+			loadArticle("Petite_Ceinture", "Petite Ceinture");
 			break;
 		case "White_Pass":
-			loadArticle();
+			loadArticle("White_Pass", "White Pass");
 			break;
 		default :
 			console.log("ERROR, page non reconnue.");
@@ -523,19 +522,18 @@ function loadCaroussel(_callback){
 		makeResponsiveCarousel();
 		_callback();
 	}
+	
 	owl.trigger('owl.play',6000);
+
 }
 
 function makeResponsiveCarousel(){
   var imgH = $(".owl-stage-outer").height();
   if(imgH == undefined)
     return ;
-  /*var delta = imgH - $("#carousel").height();
-  if(delta>0){
-  	$("#articletitle").css("top", '37.5%');
-  	return;
-  }*/
   $("#articletitle").css("top", imgH/2);
+  $(".overlay").css("height", imgH);
+
 }
 
 function replaceSachaDots(){
@@ -559,10 +557,10 @@ function affLoadingPage(){
 	$('#menu').removeClass('dark');
 }
 
-function loadArticle(){
+function loadArticle(page, title){
 	affLoadingPage();
 	setTimeout(function(){
-		load_template_page("article", "Title", function(){
+		load_template_page(page, title, function(){
 			loadCaroussel(function(){
 				setTimeout(function(){
 				    $(".se-pre-con").removeClass("visible");
